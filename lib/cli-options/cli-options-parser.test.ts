7086336ffdbdc6cdc0be850eac930f86a5027554
hdorgeval <henri.dorgeval@outlook.fr>
2018-12-22 23:32:32 +0100

    test(cli): add test on CLI options parsing

--- lib/cli-options/cli-options-parser.test.ts
+++ lib/cli-options/cli-options-parser.test.ts
@@ -1,5 +1,17 @@
 import { getCliOptions } from "./cli-options-parser";
 
+const argv = {
+  'npm run release-checker -- --help': [ 
+      '/usr/local/bin/node',
+      '/Users/user_name/projects/release-checker/build/bin/release-checker',
+      '--help' ],
+
+  'npx release-checker --help': [ 
+      '/usr/local/bin/node',
+      '/Users/user_name/.npm/_npx/49244/bin/release-checker',
+      '--help' ],
+}
+
 describe('CLI options parsing', () => {
   let nativeProcessArgv: string[];
 
@@ -12,9 +24,18 @@ describe('CLI options parsing', () => {
 
   test('It should parse --help on command `npm run release-checker -- --help` ', () => {
     // Given
-    process.argv = [ '/usr/local/bin/node',
-    '/Users/HDO/VSCodeProjects/release-checker/build/bin/release-checker',
-    '--help' ];
+    process.argv = argv['npm run release-checker -- --help'];
+
+    // When
+    const options = getCliOptions();
+
+    // Then
+    expect(options["--help"]).toBe(true);
+  });
+
+  test('It should parse --help on command `npx release-checker --help` ', () => {
+    // Given
+    process.argv = argv['npx release-checker --help']
 
     // When
     const options = getCliOptions();
