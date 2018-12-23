716e897c97e234966f7d21de065aee41ce5ce780
hdorgeval <henri.dorgeval@outlook.fr>
2018-12-23 16:07:56 +0100

    feat(utils): be able to execute a nodejs command

---
+++ lib/utils/exec-sync.ts
@@ -0,0 +1,10 @@
+import {execSync} from 'child_process';
+
+export function exec(command: string) {
+    try {
+        const result = execSync(command, { encoding: 'utf8' });
+        return result.toString();
+    } catch (error) {
+        return error.message;
+    }
+};
