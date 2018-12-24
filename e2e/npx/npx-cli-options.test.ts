b2916d4b0bbfbc81434e4c0b65b0853ba3d20508
hdorgeval <henri.dorgeval@outlook.fr>
2018-12-24 13:59:37 +0100

    test(e2e): add integration test for npx usage

---
+++ e2e/npx/npx-cli-options.test.ts
@@ -0,0 +1,33 @@
+
+import {readFileSync} from 'fs';
+import {join} from 'path';
+import { exec } from '../../lib/utils/exec-sync';
+import { usage } from '../../lib/cli-options/usage';
+
+describe('npx - CLI options parsing', () => {
+  let nativeCwd: string;
+  let packageFilename: string;
+  let packageFilepath: string;
+  beforeAll(() => {
+    const packageDotJson: any = JSON.parse(readFileSync(join(process.cwd(), 'package.json')).toString());
+    packageFilename = `${packageDotJson.name}-${packageDotJson.version}.tgz`;
+    packageFilepath  = join(process.cwd(),packageFilename);
+  })
+  beforeEach(() => {
+    nativeCwd = process.cwd();
+  });
+  afterEach(() => {
+    process.chdir(nativeCwd);
+  });
+
+  test('It should detect no options on command `npx release-checker` ', () => {
+    // Given
+    const command = `npx ${packageFilepath}`;
+
+    // When
+    const result = exec(command);
+   
+    // Then
+    expect(result).toContain(usage);
+  });
+});
