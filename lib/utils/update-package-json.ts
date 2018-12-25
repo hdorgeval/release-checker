6752139281037d268e12bca55ab57032289a52cd
hdorgeval <henri.dorgeval@outlook.fr>
2018-12-25 20:24:05 +0100

    test(utils): add test

--- lib/utils/update-package-json.ts
+++ lib/utils/update-package-json.ts
@@ -4,9 +4,6 @@ import { readPackageDotJsonInCurrentWorkingDirectory } from './read-package-json
 
 export function addScriptInPackageDotJsonOfCurrentWorkingDirectory(key: string, script: string): void {
   const pkg = readPackageDotJsonInCurrentWorkingDirectory();
-  if (!pkg) {
-    return;
-  }
   pkg.scripts = pkg.scripts || {};
   pkg.scripts[key] = script;
   writeFileSync(join(process.cwd(), 'package.json'), JSON.stringify(pkg, null, 2));
