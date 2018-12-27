a1c9e4302547e48dd3e0b57005c8383b6214d5b7
hdorgeval <henri.dorgeval@outlook.fr>
2018-12-27 13:09:18 +0100

    test(validator): add tests for package.json validator

---
+++ lib/validators/package-json-validator/validator.test.ts
@@ -0,0 +1,23 @@
+import { packageJsonValidator } from './index';
+
+describe.only('package.json validator', () => {
+  test('It should always run` ', () => {
+    // Given
+    const validator = packageJsonValidator;
+
+    // When
+    const result = validator.canRun && validator.canRun();
+
+    // Then
+    expect(result).toBe(true);
+  });
+
+  test('It should throw an exception when not implemented` ', () => {
+    // Given
+    const validator = packageJsonValidator;
+
+    // When
+    // Then
+    expect(() => validator.run && validator.run()).toThrowError(new Error('Validator not implemented'));
+  });
+});
