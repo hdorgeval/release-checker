40a3dcf041a388580b7a15fb9a6d34bf56c8b581
hdorgeval <henri.dorgeval@outlook.fr>
2019-01-01 18:03:48 +0100

    test(validator): add test to the package.json validator

--- lib/validators/package-json-validator/validator.test.ts
+++ lib/validators/package-json-validator/validator.test.ts
@@ -1,6 +1,7 @@
 import { mkdirSync, writeFileSync } from 'fs';
 import { join } from 'path';
 import { exec } from '../../utils/exec-sync';
+import { PackageDotJson } from '../../utils/read-package-json';
 import { packageJsonValidator } from './index';
 
 let nativeProcessArgv: string[];
@@ -52,4 +53,36 @@ test('It should throw an exception when package.json is badly formed', () => {
   // Then
   const expectedError = new Error(`package.json file in '${tempFolder}' is badly formed`);
   expect(() => validator.run && validator.run()).toThrowError(expectedError);
+});
+
+test('It should return a validation error when name section of package.json is undefined', () => {
+  // Given
+  const validator = packageJsonValidator;
+  const pkg: Partial<PackageDotJson> = { version: '1.0.0', scripts: {} };
+  const pkgFilepath = join(tempFolder, 'package.json');
+  writeFileSync(pkgFilepath, JSON.stringify(pkg, null, 2));
+
+  // When
+  const result = validator.run && validator.run();
+
+  // Then
+  expect(Array.isArray(result)).toBe(true);
+  expect(result && result.length).toBe(1);
+  expect(result && result[0]).toEqual({ reason: 'package.json has no name defined' });
+});
+
+test('It should return a validation error when version section of package.json is undefined', () => {
+  // Given
+  const validator = packageJsonValidator;
+  const pkg: Partial<PackageDotJson> = { name: 'testing-repo', scripts: {} };
+  const pkgFilepath = join(tempFolder, 'package.json');
+  writeFileSync(pkgFilepath, JSON.stringify(pkg, null, 2));
+
+  // When
+  const result = validator.run && validator.run();
+
+  // Then
+  expect(Array.isArray(result)).toBe(true);
+  expect(result && result.length).toBe(1);
+  expect(result && result[0]).toEqual({ reason: 'package.json has no version defined' });
 });
