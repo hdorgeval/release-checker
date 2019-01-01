9890c7832555e2f641c017b2df3cdc66a48c2806
hdorgeval <henri.dorgeval@outlook.fr>
2019-01-01 14:05:02 +0100

    test(utils): add test

--- lib/utils/read-package-json.test.ts
+++ lib/utils/read-package-json.test.ts
@@ -1,4 +1,4 @@
-import { mkdirSync } from 'fs';
+import { mkdirSync, writeFileSync } from 'fs';
 import { join } from 'path';
 import { exec } from './exec-sync';
 import { ensureThat } from './read-package-json';
@@ -33,5 +33,21 @@ test('It should ensure that package.json is not missing` ', () => {
       .inDirectory(tempFolder)
       .canBeRead()
       .asString(),
+  ).toThrowError(expectedError);
+});
+
+test('It should ensure that package.json is readable as JSON` ', () => {
+  // Given
+  const pkgFilepath = join(tempFolder, 'package.json');
+  writeFileSync(pkgFilepath, '<bad json>');
+
+  // When
+  // Then
+  const expectedError = new Error(`package.json file in '${tempFolder}' is badly formed`);
+  expect(() =>
+    ensureThat('package.json')
+      .inDirectory(tempFolder)
+      .canBeRead()
+      .asJson(),
   ).toThrowError(expectedError);
 });
