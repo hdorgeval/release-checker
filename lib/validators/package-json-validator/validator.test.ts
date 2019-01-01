16ab77b2ec7843084562d4e044fe1bb8c48b5bd8
hdorgeval <henri.dorgeval@outlook.fr>
2019-01-01 16:34:44 +0100

    test(validator): add test to the package.json validator

--- lib/validators/package-json-validator/validator.test.ts
+++ lib/validators/package-json-validator/validator.test.ts
@@ -1,4 +1,4 @@
-import { mkdirSync } from 'fs';
+import { mkdirSync, writeFileSync } from 'fs';
 import { join } from 'path';
 import { exec } from '../../utils/exec-sync';
 import { packageJsonValidator } from './index';
@@ -39,5 +39,17 @@ test('It should throw an exception when package.json is missing', () => {
   // When
   // Then
   const expectedError = new Error(`package.json file is missing in '${tempFolder}' `);
+  expect(() => validator.run && validator.run()).toThrowError(expectedError);
+});
+
+test('It should throw an exception when package.json is badly formed', () => {
+  // Given
+  const validator = packageJsonValidator;
+  const pkgFilepath = join(tempFolder, 'package.json');
+  writeFileSync(pkgFilepath, '<bad json>');
+
+  // When
+  // Then
+  const expectedError = new Error(`package.json file in '${tempFolder}' is badly formed`);
   expect(() => validator.run && validator.run()).toThrowError(expectedError);
 });
