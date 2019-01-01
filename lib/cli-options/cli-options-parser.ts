c9a8cc2119434874b0c371c59a8cd618741edbe5
hdorgeval <henri.dorgeval@outlook.fr>
2019-01-01 19:57:31 +0100

    feat(cli-options): add --test, -t to command-line options

--- lib/cli-options/cli-options-parser.ts
+++ lib/cli-options/cli-options-parser.ts
@@ -4,10 +4,15 @@ export interface ReleaseCheckerOptions {
   [index: string]: string | boolean;
   '--help': boolean;
   '--package.json': boolean;
+  '--test': boolean;
 }
 
 export function getCliOptions(): ReleaseCheckerOptions {
   const args = minimist(process.argv.slice(2));
-  const options: ReleaseCheckerOptions = { '--help': args.help || args.h || false, '--package.json': true };
+  const options: ReleaseCheckerOptions = {
+    '--help': args.help || args.h || false,
+    '--package.json': true,
+    '--test': args.test || args.t || false,
+  };
   return options;
 }
