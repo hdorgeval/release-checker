5f28ffed76b37ac5de30db6cebb0b45fce22236c
hdorgeval <henri.dorgeval@outlook.fr>
2019-01-09 13:21:49 +0100

    feat(utils): add helper method to check if command 'npm pack' has a JSON reporter

--- lib/utils/npm-infos.test.ts
+++ lib/utils/npm-infos.test.ts
@@ -1,4 +1,5 @@
-import { getCurrentNpmVersion } from './npm-infos';
+import * as execModule from './exec-sync';
+import { currentNpmVersion, getCurrentNpmVersion, NpmVersionInfo } from './npm-infos';
 
 test('It should get current npm version', () => {
   // Given
@@ -13,11 +14,45 @@ test('It should get current npm version', () => {
 test('It should throw an error when result of command `npm version --json` is not parseable', () => {
   // Given
   // When
-  jest.spyOn(global.JSON, 'parse').mockImplementation(() => {
+  const spy = jest.spyOn(global.JSON, 'parse').mockImplementation(() => {
     throw new Error('yo');
   });
 
   // Then
   const expectedError = new Error(`Cannot parse the result of command 'npm version --json'`);
   expect(() => getCurrentNpmVersion()).toThrowError(expectedError);
+  spy.mockRestore();
+});
+
+test('It should use json formatter of command `npm pack` when npm version is 5.9.0', () => {
+  // Given
+  const spy = jest.spyOn(execModule, 'exec').mockImplementation(() => {
+    const npmInfo: NpmVersionInfo = { npm: '5.9.0', node: '9.2.0' };
+    return JSON.stringify(npmInfo, null, 2);
+  });
+
+  // When
+  const result = currentNpmVersion()
+    .canReportInJson()
+    .packCommand();
+
+  // Then
+  expect(result).toBe(true);
+  spy.mockRestore();
+});
+
+test('It should not use json formatter of command `npm pack` when npm version is 5.8.0', () => {
+  // Given
+  const spy = jest.spyOn(execModule, 'exec').mockImplementation(() => {
+    const npmInfo: NpmVersionInfo = { npm: '5.8.0', node: '9.2.0' };
+    return JSON.stringify(npmInfo, null, 2);
+  });
+  // When
+  const result = currentNpmVersion()
+    .canReportInJson()
+    .packCommand();
+
+  // Then
+  expect(result).toBe(false);
+  spy.mockRestore();
 });
--- lib/utils/npm-infos.ts
+++ lib/utils/npm-infos.ts
@@ -1,4 +1,7 @@
+import * as semver from 'semver';
 import { exec } from './exec-sync';
+
+const NPM_VERSION_WITH_PACK_JSON_REPORTER = '5.9.0';
 
 export function getCurrentNpmVersion(): string {
   const result = exec('npm version --json');
@@ -13,4 +16,21 @@ export interface NpmVersionInfo {
   [key: string]: string;
   npm: string;
   node: string;
+}
+
+export function npmPackCanReportInJson(version: string) {
+  return semver.gte(version, NPM_VERSION_WITH_PACK_JSON_REPORTER);
+}
+
+export function currentNpmVersion() {
+  const currentVersion = getCurrentNpmVersion();
+  return {
+    canReportInJson() {
+      return {
+        packCommand(): boolean {
+          return npmPackCanReportInJson(currentVersion);
+        },
+      };
+    },
+  };
 }
