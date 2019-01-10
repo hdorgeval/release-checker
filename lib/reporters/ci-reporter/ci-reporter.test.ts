5899ac86396b1da41daa917341cecf654f7d9e66
hdorgeval <henri.dorgeval@outlook.fr>
2019-01-10 22:28:55 +0100

    test(ci-reporter): test warnings report

--- lib/reporters/ci-reporter/ci-reporter.test.ts
+++ lib/reporters/ci-reporter/ci-reporter.test.ts
@@ -29,3 +29,31 @@ test('It should show validation errors` ', () => {
   expect(output[3]).toContain(error2.reason);
   expect(output[4]).toContain(error3.reason);
 });
+
+test('It should show validation warnings` ', () => {
+  // Given
+  const warning1: ValidationWarning = { reason: 'warning from validator2', severity: 'warning' };
+  const warning2: ValidationWarning = { reason: 'warning 1 from validator3', severity: 'warning' };
+  const warning3: ValidationWarning = { reason: 'warning 2 from validator3', severity: 'warning' };
+  const warning4: ValidationWarning = { reason: 'warning from validator4', severity: 'warning' };
+  const error1: ValidationError = { reason: 'warning from validator4', severity: 'error' };
+  const validator1: Partial<Validator> = { hasWarnings: false, hasErrors: true };
+  const validator2: Partial<Validator> = { hasWarnings: true, warnings: [warning1] };
+  const validator3: Partial<Validator> = { hasWarnings: true, warnings: [warning2, warning3] };
+  const validator4: Partial<Validator> = { hasErrors: true, hasWarnings: true, errors: [error1], warnings: [warning4] };
+
+  const validators = [validator1, validator2, validator3, validator4];
+
+  // When
+  const output: string[] = [];
+  jest.spyOn(global.console, 'log').mockImplementation((...args) => output.push(...args));
+  ciReporter.reportValidationWarningsOf(validators);
+
+  // Then
+  expect(output.length).toBe(6);
+  expect(output[1]).toContain('WARNINGS');
+  expect(output[2]).toContain(warning1.reason);
+  expect(output[3]).toContain(warning2.reason);
+  expect(output[4]).toContain(warning3.reason);
+  expect(output[5]).toContain(warning4.reason);
+});
