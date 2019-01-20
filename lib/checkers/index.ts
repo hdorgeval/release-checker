ae4de9b183bd57cbd4f91f33736dc865cf6acabc
hdorgeval <henri.dorgeval@outlook.fr>
2019-01-20 23:01:58 +0100

    feat(checkers): enable git branch checker

--- lib/checkers/index.ts
+++ lib/checkers/index.ts
@@ -1,6 +1,12 @@
 import { Checker } from './common/checker-interface';
+import { gitBranchChecker } from './git-branch-checker/index';
 import { npmTestChecker } from './npm-test-checker/index';
 import { packageJsonChecker } from './package-json-checker/index';
 import { sensitiveDataChecker } from './sensitive-data-checker';
 
-export const checkers: Array<Partial<Checker>> = [packageJsonChecker, npmTestChecker, sensitiveDataChecker];
+export const checkers: Array<Partial<Checker>> = [
+  packageJsonChecker,
+  npmTestChecker,
+  sensitiveDataChecker,
+  gitBranchChecker,
+];
