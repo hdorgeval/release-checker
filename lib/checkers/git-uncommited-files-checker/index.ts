87c0f5db45208d0852baa2fdbb533382bc9f108f
hdorgeval <henri.dorgeval@outlook.fr>
2019-01-26 19:28:21 +0100

    feat(checkers): create uncommited-files checker

---
+++ lib/checkers/git-uncommited-files-checker/index.ts
@@ -0,0 +1,23 @@
+import { Checker, ValidationError, ValidationWarning } from '../common/checker-interface';
+import { getUncommitedFiles, gitIsInstalled } from '../common/git';
+
+export const gitUncommitedFilesChecker: Partial<Checker> = {
+  canRun: () => gitIsInstalled(),
+  cliOption: '--uncommited-files',
+  id: 'git-uncommited-files-checker',
+  run: () => validate(),
+  statusToDisplayWhileValidating: 'Checking there are no uncommited files',
+  whyCannotRun: () => `git not found. Run 'npm doctor' for more details`,
+};
+
+function validate(): Array<ValidationError | ValidationWarning> {
+  const uncommitedFiles = getUncommitedFiles();
+  const validationErrorsAndWarnings: Array<ValidationError | ValidationWarning> = [];
+  uncommitedFiles.forEach((filepath) => {
+    validationErrorsAndWarnings.push({
+      reason: `File '${filepath}' is uncommited`,
+      severity: 'error',
+    });
+  });
+  return validationErrorsAndWarnings;
+}
